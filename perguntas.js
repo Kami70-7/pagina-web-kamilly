dceaf5301230a1adb99c7af73ae2088e454ca159
Kamilly <cordeiro.santos.kamilly@escola.pr.gov.br>
2025-05-28 23:23:22 +0000

    Novas perguntas

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartão(
+    'Geografia',
+    'Qual é o maior oceano so mundo?',
+    'O maior Oceano é o Oceano Pacífico'
+)
+
+criaCartão(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Álvares Cabral'
+)
+
+criaCartão(
+    'Educação física',
+    'Qual é o único jogador de futebol que pode pegar a bola com a mão?',
+    'É o goleiro'
+)
+
+criaCartão(
+    'Matemática',
+    'Qual é a raíz quadrada de 9?',
+    'A raiz quadrada é 3'
+)
+
+criaCartão(
+    'Biologia'
+    'Qual é a cor da clorofia?',
+    'A cor é verde'
+)
+
+criaCartão(
+    'Química',
+    'Quantos elementos tem na tabela periódica?',
+    'Tem 118 elementos'
+)
+
+criaCartão(
+    'Língua portuguesa'
+    'Qual é o antônimo de bom?',
+    'O antõnimo é mau'
+)
+
+criaCartão(
+    'Inglês',
+    'O que a palavra "end" significa?',
+    'Significa Fim'
+)
+
+criaCartão(
+    'Espanhol',
+    'Como diz olá em espanhol?',
+    'Oi em espanhol é HOLA'
+)
